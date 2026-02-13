6106ddc9a9f5ca7b6e6289db20ad99adaa2c35fb
Andrea <danielleholandaa@gmail.com>
2026-02-13 15:14:22 -0300

    Adicionando fotos, vídeos e correção no CSS

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function toggleslider() {
+    const slider = document.getElementById("meuSlider");
+    if (slider.style.width === "250px") {
+        slider.style.width = "0";
+    }
+    else {
+        slider.style.width = "250px";
+
+
+    }
+
+
+
+
+}
+
+
